21bc67e009105bcdb866ba665ba176e74af49ca3
Daniel Bramich <dmb20@aduae386-lap-v.abudhabi.nyu.edu>
2019-09-11 08:41:40 +0000

    Added configuration file

---
+++ config/config.py
@@ -0,0 +1,13 @@
+__author__ = 'Dan Bramich'
+
+
+##############################################
+# General pipeline configuration parameters
+
+# Location of original data sources
+original_detectors_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Locations/detectors.csv'
+original_links_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Locations/links.csv'
+original_measurements_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Measurements/loops_measurements.csv'
+
+# Output directory for results
+output_dir = '/data/dmb20/Loop.Detector.Data.Analysis.MFD.Project.1/Results'
