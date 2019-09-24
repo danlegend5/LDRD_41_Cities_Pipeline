fbf56789f76431fc4311c32a3d29a52f9d926857
Daniel Bramich <dmb20@nyu.edu>
2019-09-24 09:47:16 +0000

    Updated config file

--- config/config.py
+++ config/config.py
@@ -7,7 +7,8 @@ __author__ = 'Dan Bramich'
 # Location of original data sources
 original_detectors_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Locations/detectors.csv'
 original_links_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Locations/links.csv'
-original_measurements_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Measurements/loops_measurements.csv'
+original_raw_measurements_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Measurements/loops_measurements.csv'
+original_arima_measurements_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loop.Detector.Measurements/loops_measurements_arima.csv'
 
 # Output directory for results
 output_dir = '/data/dmb20/Loop.Detector.Data.Analysis.MFD.Project.1/Results'
