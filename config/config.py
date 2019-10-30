4b3e7de30089d37b83391e86e8b63e96eb56d051
Daniel Bramich <dmb20@nyu.edu>
2019-10-30 11:55:01 +0000

    Started developing script to estimate a spatially varying MFD for a city

--- config/config.py
+++ config/config.py
@@ -12,3 +12,8 @@ original_measurements_arima_file = '/data/dmb20/Loop.Detector.Data.41.Cities/Loo
 
 # Output directory for results
 output_dir = '/data/dmb20/Loop.Detector.Data.Analysis.MFD.Project.1/Results'
+
+# Mean radius (km) of the Earth to be used for the approximate conversion of sets of longitude and latitude
+# coordinates within a city to sets of x and y Cartesian coordinates (taken from the International Union of
+# Geodesy and Geophysics - IUGG)
+radius_earth = 6371.0
