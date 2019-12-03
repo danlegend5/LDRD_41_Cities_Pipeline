eb339fe40730d9bb5a62688a37fd53d43e7332fe
Daniel Bramich <dmb20@nyu.edu>
2019-12-03 12:10:41 +0000

    Finished developing stage 2

--- s2_convert_data_to_2D_format.py
+++ s2_convert_data_to_2D_format.py
@@ -13,6 +13,7 @@ import glob
 import numpy
 import os
 import shutil
+from astropy.table import Column
 from astropy.table import Table
 from astropy.time import TimeDelta
 from LDRD_41_Cities_Pipeline.config import config
@@ -64,6 +65,7 @@ file_list.sort()
 print('No. of files: ' + str(len(file_list)))
 
 # For each loop detector locations data file
+empty_str = '                                                                                '
 for file in file_list:
 
     # Read in the loop detector locations data file
@@ -110,13 +112,19 @@ for file in file_list:
     if data_source != 'LD.Flow.LD.Speed': occ_im = numpy.zeros((nld_locations, ntime_bins), dtype = numpy.float64) - 1.0
     error_im = numpy.ones((nld_locations, ntime_bins), dtype = numpy.int32)
     if data_source != 'LD.Flow.LD.Occupancy': speed_im = numpy.zeros((nld_locations, ntime_bins), dtype = numpy.float64) - 1.0
+    arima_flow_im = numpy.zeros((nld_locations, ntime_bins), dtype = numpy.float64) - 1.0
+    if data_source != 'LD.Flow.LD.Speed': arima_occ_im = numpy.zeros((nld_locations, ntime_bins), dtype = numpy.float64) - 1.0
+    arima_error_im = numpy.ones((nld_locations, ntime_bins), dtype = numpy.int32)
+    if data_source != 'LD.Flow.LD.Occupancy': arima_speed_im = numpy.zeros((nld_locations, ntime_bins), dtype = numpy.float64) - 1.0
+    arima_data_tag = 0
     for i in range(nld_locations):
 
         # Determine the full directory path and filename of the corresponding loop detector measurements
         # data file (raw)
-        print('Reading in the loop detector measurements data (raw) and incorporating them into the grid for the detector: ' + ld_locations_table['DETECTOR_ID'][i])
-        ld_measurements_file_raw = 'measurements.raw.' + data_source + '.' + country_name + '.' + city_name + '.' + ld_locations_table['DETECTOR_ID'][i] + '.fits'
-        ld_measurements_file_raw = os.path.join(config.output_dir, 's1.Loop.Detector.Measurements.Raw', data_source, country_name, city_name, ld_locations_table['DETECTOR_ID'][i], ld_measurements_file_raw)
+        curr_detector_id = ld_locations_table['DETECTOR_ID'][i]
+        print('Reading in the loop detector measurements data (raw and ARIMA) and incorporating them into the grid for the detector: ' + curr_detector_id)
+        ld_measurements_file_raw = 'measurements.raw.' + data_source + '.' + country_name + '.' + city_name + '.' + curr_detector_id + '.fits'
+        ld_measurements_file_raw = os.path.join(config.output_dir, 's1.Loop.Detector.Measurements.Raw', data_source, country_name, city_name, curr_detector_id, ld_measurements_file_raw)
 
         # Read in the corresponding loop detector measurements data file (raw)
         ld_measurements_table_raw = Table.read(ld_measurements_file_raw, format = 'fits')
@@ -132,7 +140,8 @@ for file in file_list:
 
             # Determine the mid-point of the aggregation time-interval for the current loop detector
             # measurement
-            curr_time_stamp = general_functions.convert_time_stamp_to_time_object(ld_measurements_table_raw['DATE'][j] + 'T' + '{:05d}'.format(ld_measurements_table_raw['INTERVAL_START'][j]).strip())
+            date_interval_start_str = ld_measurements_table_raw['DATE'][j] + 'T' + '{:05d}'.format(ld_measurements_table_raw['INTERVAL_START'][j]).strip()
+            curr_time_stamp = general_functions.convert_time_stamp_to_time_object(date_interval_start_str)
             curr_time_stamp += curr_half_interval
 
             # Determine which time bin the current loop detector measurement should be assigned to
@@ -140,13 +149,12 @@ for file in file_list:
             if curr_time_diff >= 0:
                 curr_time_bin_sub += numpy.int32(numpy.floor(curr_time_diff/curr_interval)) + 1
 
-            # Check that the designated time bin has not been filled previously for the current loop
+            # Check that the designated time bin has not already been filled for the current loop
             # detector
             if error_im[i, curr_time_bin_sub] == 0:
-#######################################################
-                print('PROBLEM! ', data_source, country_name, city_name, ld_locations_table['DETECTOR_ID'][i], curr_time_stamp)
+                print('ERROR - The designated time bin for the current loop detector measurement (raw) has already been filled (CITY, DETECTOR_ID, DATE, INTERVAL_START): ' + city_name + ', ' + \
+                       curr_detector_id + ', ' + ld_measurements_table_raw['DATE'][j] + ', ' + str(ld_measurements_table_raw['INTERVAL_START'][j]))
                 exit()
-#######################################################
 
             # Assign the current loop detector measurement to the designated time bin
             flow_im[i, curr_time_bin_sub] = ld_measurements_table_raw['FLOW'][j]
@@ -154,7 +162,54 @@ for file in file_list:
             error_im[i, curr_time_bin_sub] = 0
             if data_source != 'LD.Flow.LD.Occupancy': speed_im[i, curr_time_bin_sub] = ld_measurements_table_raw['SPEED'][j]
 
-    # Write out the two-dimensional arrays of loop detector measurements (raw) for the current city
+        # Determine the full directory path and filename of the corresponding loop detector measurements
+        # data file (ARIMA)
+        ld_measurements_file_arima = 'measurements.ARIMA.' + data_source + '.' + country_name + '.' + city_name + '.' + curr_detector_id + '.fits'
+        ld_measurements_file_arima = os.path.join(config.output_dir, 's1.Loop.Detector.Measurements.ARIMA', data_source, country_name, city_name, curr_detector_id, ld_measurements_file_arima)
+
+        # If the corresponding loop detector measurements data file (ARIMA) does not exist, then move on to
+        # the next loop detector
+        if not os.path.exists(ld_measurements_file_arima): continue
+
+        # Read in the corresponding loop detector measurements data file (ARIMA)
+        ld_measurements_table_arima = Table.read(ld_measurements_file_arima, format = 'fits')
+        nld_measurements_arima = len(ld_measurements_table_arima)
+        arima_data_tag = 1
+
+        # For each loop detector measurement
+        curr_time_bin_sub = 0
+        for j in range(nld_measurements_arima):
+
+            # If the current loop detector measurement is flagged with "ERROR_FLAG = 1", then move on to
+            # the next loop detector measurement
+            if ld_measurements_table_arima['ERROR_FLAG'][j] == 1: continue
+
+            # Determine the mid-point of the aggregation time-interval for the current loop detector
+            # measurement
+            date_interval_start_str = ld_measurements_table_arima['DATE'][j] + 'T' + '{:05d}'.format(ld_measurements_table_arima['INTERVAL_START'][j]).strip()
+            curr_time_stamp = general_functions.convert_time_stamp_to_time_object(date_interval_start_str)
+            curr_time_stamp += curr_half_interval
+
+            # Determine which time bin the current loop detector measurement should be assigned to
+            curr_time_diff = curr_time_stamp - time_bin_limits[curr_time_bin_sub + 1]
+            if curr_time_diff >= 0:
+                curr_time_bin_sub += numpy.int32(numpy.floor(curr_time_diff/curr_interval)) + 1
+
+            # Check that the designated time bin has not already been filled for the current loop
+            # detector
+            if arima_error_im[i, curr_time_bin_sub] == 0:
+                print('ERROR - The designated time bin for the current loop detector measurement (ARIMA) has already been filled (CITY, DETECTOR_ID, DATE, INTERVAL_START): ' + city_name + ', ' + \
+                       curr_detector_id + ', ' + ld_measurements_table_arima['DATE'][j] + ', ' + str(ld_measurements_table_arima['INTERVAL_START'][j]))
+                exit()
+
+            # Assign the current loop detector measurement to the designated time bin
+            arima_flow_im[i, curr_time_bin_sub] = ld_measurements_table_arima['ARIMA_FLOW'][j]
+            if data_source != 'LD.Flow.LD.Speed': arima_occ_im[i, curr_time_bin_sub] = ld_measurements_table_arima['ARIMA_OCCUPANCY'][j]
+            arima_error_im[i, curr_time_bin_sub] = 0
+            if data_source != 'LD.Flow.LD.Occupancy': arima_speed_im[i, curr_time_bin_sub] = ld_measurements_table_arima['ARIMA_SPEED'][j]
+
+    # Write out the two-dimensional arrays of loop detector measurements for the current city
+    print('Writing out the two-dimensional arrays of loop detector measurements...')
     flow_file = os.path.join(output_dir_ld_data, 'measurements.raw.' + data_source + '.' + country_name + '.' + city_name + '.flow.fits')
     astropy.io.fits.writeto(flow_file, flow_im, checksum = True)
     if data_source != 'LD.Flow.LD.Speed':
@@ -165,10 +220,37 @@ for file in file_list:
     if data_source != 'LD.Flow.LD.Occupancy':
         speed_file = os.path.join(output_dir_ld_data, 'measurements.raw.' + data_source + '.' + country_name + '.' + city_name + '.speed.fits')
         astropy.io.fits.writeto(speed_file, speed_im, checksum = True)
+    if arima_data_tag == 1:
+        arima_flow_file = os.path.join(output_dir_ld_data, 'measurements.ARIMA.' + data_source + '.' + country_name + '.' + city_name + '.flow.fits')
+        astropy.io.fits.writeto(arima_flow_file, arima_flow_im, checksum = True)
+        if data_source != 'LD.Flow.LD.Speed':
+            arima_occ_file = os.path.join(output_dir_ld_data, 'measurements.ARIMA.' + data_source + '.' + country_name + '.' + city_name + '.occupancy.fits')
+            astropy.io.fits.writeto(arima_occ_file, arima_occ_im, checksum = True)
+        arima_error_file = os.path.join(output_dir_ld_data, 'measurements.ARIMA.' + data_source + '.' + country_name + '.' + city_name + '.error_flag.fits')
+        astropy.io.fits.writeto(arima_error_file, arima_error_im, checksum = True)
+        if data_source != 'LD.Flow.LD.Occupancy':
+            arima_speed_file = os.path.join(output_dir_ld_data, 'measurements.ARIMA.' + data_source + '.' + country_name + '.' + city_name + '.speed.fits')
+            astropy.io.fits.writeto(arima_speed_file, arima_speed_im, checksum = True)
 
+    # Prepare a data table containing the time stamps defining the time grid
+    timestamps_table_file = os.path.join(output_dir_ld_data, 'timestamps.' + data_source + '.' + country_name + '.' + city_name + '.fits')
+    print('Writing out the time-stamps data table: ' + timestamps_table_file)
+    timestamps_table = Table([Column(data = [empty_str]*ntime_bins, name = 'LOWER_BIN_LIMIT_TIMESTAMP'),
+                              Column(data = [empty_str]*ntime_bins, name = 'UPPER_BIN_LIMIT_TIMESTAMP'),
+                              Column(data = [empty_str]*ntime_bins, name = 'BIN_MIDPOINT_TIMESTAMP'),
+                              Column(data = [empty_str]*ntime_bins, name = 'LOWER_BIN_LIMIT_MJD'),
+                              Column(data = [empty_str]*ntime_bins, name = 'UPPER_BIN_LIMIT_MJD'),
+                              Column(data = [empty_str]*ntime_bins, name = 'BIN_MIDPOINT_MJD')])
+    for i in range(ntime_bins):
+        timestamps_table['LOWER_BIN_LIMIT_TIMESTAMP'][i] = str(time_bin_limits[i])
+        timestamps_table['UPPER_BIN_LIMIT_TIMESTAMP'][i] = str(time_bin_limits[i + 1])
+        timestamps_table['BIN_MIDPOINT_TIMESTAMP'][i] = str(time_mid_points[i])
+        timestamps_table['LOWER_BIN_LIMIT_MJD'][i] = '{:25.8f}'.format(time_bin_limits[i].mjd).strip()
+        timestamps_table['UPPER_BIN_LIMIT_MJD'][i] = '{:25.8f}'.format(time_bin_limits[i + 1].mjd).strip()
+        timestamps_table['BIN_MIDPOINT_MJD'][i] = '{:25.8f}'.format(time_mid_points[i].mjd).strip()
 
-#### ABOVE FULLY READ AND TESTED
-
+    # Write out the time-stamps data table
+    timestamps_table.write(timestamps_table_file, format = 'fits')
 
 # Finish
 print('')
